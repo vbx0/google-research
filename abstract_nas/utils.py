b54231e793758ea3cea5997fcadffe1a32a6e69b
Kynji <109643440+vbx0@users.noreply.github.com>
2023-04-29 09:13:18 -0400

    Update utils.py

--- abstract_nas/utils.py
+++ abstract_nas/utils.py
@@ -19,88 +19,101 @@ import contextlib
 import pickle as builtin_pickle
 from typing import Any
 
+import jax
+from absl import logging
+from tensorflow.io import gfile
+
+
+def report_memory() -> None:
+    """Prints the size and number of live buffers tracked by the backend."""
+    backend = jax.lib.xla_bridge.get_backend()
+    live_buffers = backend.live_buffers()
+    total = sum(buf.size for buf in live_buffers)
+    logging.info("num_buffers: %d | total_elements: %.2e", len(live_buffers), total)
+
+
+
+import contextlib
+import pickle as builtin_pickle
+from typing import Any
+
 from absl import logging
 import jax
-import tensorflow.io.gfile as gfile  # pylint: disable=consider-using-from-import
-
-
-def report_memory():
-  """Prints the size and number of live buffers tracked by the backend."""
-  backend = jax.lib.xla_bridge.get_backend()
-  live_buffers = backend.live_buffers()
-  total = 0
-  for buf in live_buffers:
-    total += buf.size
-  logging.info(
-      "num_buffers: %d | total_elements: %.2e", len(live_buffers), total)
+import tensorflow.io.gfile as gfile
 
 
 def canonicalize_tensor_name(name):
-  """Canonicalizes tensor names.
-
-  For an op that produces only one output, we may be refer to its output tensor
-  as either "op_name:0" or simply "op_name". This standardizes all internal
-  names as "op_name:0" to simplify the logic.
-
-  Args:
-    name: Input name to canonicalize.
-
-  Returns:
-    The canonicalized input name.
-  """
-  if ":" not in name:
-    return f"{name}:0"
-  return name
+    """Canonicalizes tensor names.
+    
+    For an op that produces only one output, we may refer to its output tensor
+    as either "op_name:0" or simply "op_name". This standardizes all internal
+    names as "op_name:0" to simplify the logic.
+    
+    Args:
+        name: Input name to canonicalize.
+    
+    Returns:
+        The canonicalized input name.
+    """
+    if ":" not in name:
+        return f"{name}:0"
+    return name
 
 
 @contextlib.contextmanager
 def _maybe_open(file, mode):
-  if isinstance(file, str):
-    with gfile.GFile(file, mode=mode) as f:
-      yield f
-  else:
-    yield file
+    if isinstance(file, str):
+        with gfile.GFile(file, mode=mode) as f:
+            yield f
+    else:
+        yield file
 
 
 def write_to_store(obj, path):
-  """Util for writing arbitrary objects to store.
+    """Util for writing arbitrary objects to a store."""
+    
+    def dumps(obj, pickle=builtin_pickle):
+        """Returns the bytes representation of a pickled object.
+        
+        Args:
+            obj: Object to pickle.
+            pickle: pickle library to use.
+        """
+        return pickle.dumps(obj)
 
-  Args:
-    obj: arbitrary object to be saved.
-    path: a path to save the object.
-  """
-  def dumps(obj, pickle=builtin_pickle):
-    """Returns the bytes representation of a pickled object.
+    def dump(obj, file, pickle=builtin_pickle):
+        """Pickles an object to a file.
+        
+        Args:
+            obj: Object to pickle.
+            file: Name of, or open file-handle to file to write pickled object to.
+            pickle: pickle library to use.
+        """
+        # Because of latency involved in CNS sequential reads, it is way faster to
+        # do f.write(dill.dumps(obj)) than dill.dump(f, obj).
+        data = dumps(obj, pickle=pickle)
+        with _maybe_open(file, mode="wb") as f:
+            f.write(data)
 
-    Args:
-      obj: Object to pickle.
-      pickle: pickle library to use.
-    """
-    return pickle.dumps(obj)
+    # In order to be robust to interruptions we first save checkpoint to the
+    # temporary file and then move to the actual path name.
+    path_tmp = path + "-TEMPORARY"
+    dump(obj, path_tmp)
+    gfile.rename(path_tmp, path, overwrite=True)
 
-  def dump(obj, file, pickle=builtin_pickle):
-    """Pickles an object to a file.
 
-    Args:
-      obj: Object to pickle.
-      file: Name of, or open file-handle to file to write pickled object to.
-      pickle: pickle library to use.
-    """
-    # Because of latency involved in CNS sequential reads, it is way faster to
-    # do f.write(dill.dumps(obj)) than dill.dump(f, obj).
-    data = dumps(obj, pickle=pickle)
-    with _maybe_open(file, mode="wb") as f:
-      f.write(data)
-
-  # In order to be robust to interruptions we first save checkpoint to the
-  # temporal file and then move to actual path name.
-  path_tmp = path + "-TEMPORARY"
-  dump(obj, path_tmp)
-  gfile.rename(path_tmp, path, overwrite=True)
+def report_memory():
+    """Prints the size and number of live buffers tracked by the backend."""
+    backend = jax.lib.xla_bridge.get_backend()
+    live_buffers = backend.live_buffers()
+    total = 0
+    for buf in live_buffers:
+        total += buf.size
+    logging.info("num_buffers: %d | total_elements: %.2e", len(live_buffers), total)
 
 
 def read_from_store(path):
-  """Util for reading arbitrary objects from store.
+  """Utility function for reading arbitrary objects from store.
 
   Args:
     path: path at which object is saved.
@@ -109,7 +122,7 @@ def read_from_store(path):
     Unpickled object.
   """
   def loads(bytes_object, pickle=builtin_pickle):
-    """Returns an object unpickled to a bytes.
+    """Returns an object unpickled from bytes.
 
     Args:
       bytes_object: bytes to unpickle.
@@ -120,12 +133,12 @@ def read_from_store(path):
     except UnicodeDecodeError:
       logging.exception(
           "Could not read pickled data. "
-          "This may be a old pickle written in Python 2, which is no longer "
+          "This may be an old pickle written in Python 2, which is no longer "
           "supported. Please use the repickle script to update your pickles.")
       raise
 
   def load(file, pickle=builtin_pickle):
-    """Returns an unpickled an object from a file.
+    """Returns an unpickled object from a file.
 
     Args:
       file: Name of, or open file-handle to file containing pickled object.
